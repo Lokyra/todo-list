f5db69d15f055ca2c615358fcd5f8cd3cb4c7436
ayoub <ayoubousbia02@gmail.com>
2023-02-27 12:35:45 +0100

    minimal todo list handling create task and delete task from user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [tasks, setTasks] = useState([])
+
+  const [newTask, setNewTask] = useState('')
+
+  const handleSubmit = (event: any) => {
+      event.preventDefault()
+      const copyTasks : any[] = [...tasks]
+
+      const id = tasks.length + 1
+      const nom = newTask
+
+      copyTasks.push({id , nom})
+
+      setTasks(copyTasks)
+      setNewTask('')
+  }
+
+  const handleChange = (event: any) => {
+      setNewTask(event.target.value)
+  }
+
+  const handleDelete = (id: number) => {
+
+      const copyTasks : any[] = [...tasks]
+
+      const copyTasksUpt = copyTasks.filter((task: any) => task.id !== id)
+
+      setTasks(copyTasksUpt)
+
+
+
+  }
+
+  
 
   return (
-  <h1>Hello World</h1>
+  <div>
+    <h1>Todo List</h1>
+    <ul>
+        {tasks.map((task: any) => (
+         <li key={task.id}>{task.nom}{" "}
+         <button onClick={() => handleDelete(task.id)}>X</button>
+         </li>
+        ))}
+    </ul>
+    <form action="submit" onSubmit={handleSubmit}>
+        <input 
+            value={newTask} 
+            type="text" 
+            placeholder="Ajouter une tache" 
+            onChange={handleChange}
+        />
+        <button type="submit">Ajouter +</button>
+    </form>
+  </div>
   )
 }
 
